bd13e6e4cc2198dc4115e2bd5d07a189fec1953d
ohdaniel <ohdaniel629@gmail.com>
2021-07-25 03:12:23 -0700

    committing server.js changes as well

--- server.js
+++ server.js
@@ -34,27 +34,37 @@ server.listen(port, () => {
 // console.log('__dirname:' + __dirname)
 
 let activeSockets = []
+let socketIdRoomNumberMap = new Map()
+let storeSocketIds = []
+let storeRoomNumbers = []
+let socketIdUserIdMap = new Map()
+let storeViewSocketIds = []
+let storeViewUserIds = []
 
 io.on('connection', (socket) => {
     var host = 'localhost';
     // var host = '192.168.86.23';
 
-    console.log('Connected on Socket ID: ' + socket.id)
+
+    const roomNumber = socket.handshake.query['roomNumber']
+    const storeViewUserId = socket.handshake.query['userId']
 
     socket.on('message', (data) => {
-        // socket.broadcast.emit('message', data)
+
         io.emit('message', data)
         console.log('server.js message')
     })
 
-    console.log('connection begin activeSockets:' + activeSockets)
     const existingSocket = activeSockets.find(existingSocket => existingSocket === socket.id)
-    console.log('existingSocket:' + existingSocket)
-    console.log(existingSocket)
-    console.log(!existingSocket)
 
     socket.emit('socketid', {
-        socketid: socket.id
+        socketid: socket.id,
+        roomNumber: roomNumber
+    })
+
+    socket.emit('store-load', {
+        socketid: socket.id,
+        roomNumber: roomNumber
     })
 
     if (!existingSocket) {
@@ -69,10 +79,58 @@ io.on('connection', (socket) => {
         })
     }
 
-    console.log('connection end activeSockets: ' + activeSockets)
+    const existingRoom = socketIdRoomNumberMap.has(socket.id)
+    var isStore = false
+    if (roomNumber) {
+        isStore = true
+    }
+
+    if (isStore && !existingRoom) {
+        socketIdRoomNumberMap.set(socket.id, roomNumber)
+        storeSocketIds.push(socket.id)
+        storeRoomNumbers.push(roomNumber)
+
+        socket.broadcast.emit('update-room-list', {
+            sockets: storeSocketIds,
+            rooms: storeRoomNumbers
+        })
+    }
+
+    const existingStoreViewUser = socketIdUserIdMap.has(socket.id)
+    var isStoreView = false
+    if (storeViewUserId) {
+        isStoreView = true
+    }
+
+    if (isStoreView && !existingStoreViewUser) {
+        socketIdUserIdMap.set(socket.id, storeViewUserId)
+        storeViewSocketIds.push(socket.id)
+        storeViewUserIds.push(storeViewUserId)
+    }
+
+    socket.emit('update-room-list', {
+        sockets: storeSocketIds,
+        rooms: storeRoomNumbers
+    })
+
+    socket.on('call-store-view-user-id', data => {
+        storeViewUserExists = false
+
+        storeViewSocketId = [...socketIdUserIdMap.entries()].filter(({1:v}) => v === data.storeViewUserId).map(([k]) => k)
+
+        if (storeViewSocketId.length > 0) {
+            storeViewUserExists = true
+        }
+
+        if (storeViewUserExists) {
+            socket.to(storeViewSocketId).emit('call-made', {
+                offer: data.offer,
+                socket: socket.id
+            })
+        }
+    })
 
     socket.on('call-user', data => {
-        console.log('call-user')
         socket.to(data.to).emit('call-made', {
             offer: data.offer,
             socket: socket.id
@@ -80,7 +138,6 @@ io.on('connection', (socket) => {
     })
 
     socket.on('make-answer', data => {
-        console.log('make-answer')
         socket.to(data.to).emit('answer-made', {
             socket: socket.id,
             answer: data.answer
@@ -88,14 +145,25 @@ io.on('connection', (socket) => {
     })
 
     socket.on('disconnect', () => {
-        console.log('disconnection begin activeSockets: ' + activeSockets)
         activeSockets = activeSockets.filter(existingSocket => existingSocket !== socket.id)
     
         socket.broadcast.emit('remove-user', {
             socketId: socket.id
         })
-        console.log('disconnection end activeSockets: ' + activeSockets)
+        
+        if (isStore) {
+            socketIdRoomNumberMap.delete(socket.id)
+            storeSocketIds = storeSocketIds.filter(existingSocket => existingSocket !== socket.id)
+            storeRoomNumbers = storeRoomNumbers.filter(existingSocket => existingSocket !== roomNumber)
+            socket.broadcast.emit('remove-room', {
+                roomNumber: socket.id
+            })
+        }
+
+        if (isStoreView) {
+            socketIdUserIdMap.delete(socket.id)
+            storeViewSocketIds = storeViewSocketIds.filter(existingStoreViewSocketId => existingStoreViewSocketId !== socket.id)
+            storeViewUserIds = storeViewUserIds.filter(existingStoreViewUserId => existingStoreViewUserId !== storeViewUserId)
+        }
     })
 })
-
-
